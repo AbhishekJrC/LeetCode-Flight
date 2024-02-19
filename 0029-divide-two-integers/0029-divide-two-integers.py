714912df0e32938bb1bbd1bb932ac0f1248ecfed
Abhishek Pradhan <156829458+AbhishekJrC@users.noreply.github.com>
2024-02-19 23:03:10 +0530

    Update 0029-divide-two-integers.py

--- 0029-divide-two-integers/0029-divide-two-integers.py
+++ 0029-divide-two-integers/0029-divide-two-integers.py
@@ -1,6 +1,6 @@
 class Solution:
   def divide(self, dividend: int, divisor: int) -> int:
-    # -2^{31} / -1 = 2^31 will overflow, so return 2^31 - 1.
+
     if dividend == -2**31 and divisor == -1:
       return 2**31 - 1
 
